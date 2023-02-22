bd0f3aa85237b938be6e3d26a7a6365a3f1ccfe4
MrHumble21 <amalikov3@students.wiut.uz>
2023-02-22 14:16:02 +0500

    commit

---
+++ run.py
@@ -0,0 +1,19 @@
+from app import app, db
+from app.models import User
+
+
+with app.app_context():
+    db.create_all()
+
+if __name__ == '__main__':
+    # manager.run()
+    app.run(host='0.0.0.0', port=8000, debug=True)
+
+
+
+
+# python run.py
+
+# to make changes db
+    # python run.py db migrate
+    # python run.py db upgrade
