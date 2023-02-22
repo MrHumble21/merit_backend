a032729f8e2d32514520bff4799a88588f85650c
MrHumble21 <amalikov3@students.wiut.uz>
2023-02-22 15:44:27 +0500

    hello

--- app/models.py
+++ app/models.py
@@ -4,17 +4,21 @@ from app import db
 class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
-    image = db.Column(db.String(150), nullable=True)
     name = db.Column(db.String(150), nullable=False)
-    birthdate = db.Column(db.String(30), nullable=False)
-    gender = db.Column(db.String(15), nullable=False)
-    country = db.Column(db.String(100), nullable=False)
-    region = db.Column(db.String(100), nullable=True)
+    region = db.Column(db.String(100), nullable=False)
     phone = db.Column(db.String(100), nullable=True)
-    email = db.Column(db.String(100), nullable=True, unique=True)
-
     def to_dict(self):  # automatic calling the dict (creating a func)
         return {column.name: getattr(self, column.name) for column in self.__table__.columns}
+
+
+class Client(db.Model):
+    __tablename__ = 'client'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(150), nullable=False)
+    region = db.Column(db.String(100), nullable=False)
+    phone = db.Column(db.String(100), nullable=True)
+
+
 
 # flask db init
 # flask db migrate
